f7b0c72b851a996e439ab0ee480971e3e68551c5
Luz H. Fischer <luzysol@gmail.com>
2015-11-20 07:13:20 -0500

    Added Live Reloading

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ var gulp = require('gulp'),
 		coffee = require('gulp-coffee'),
 		browserify = require('gulp-browserify'),
 		compass = require('gulp-compass'),
+		connect = require('gulp-connect'),
 		concat = require('gulp-concat');
 
 var coffeeSources = ['components/coffee/tagline.coffee']; 
@@ -26,10 +27,9 @@ gulp.task('js', function() {
 	gulp.src(jsSources)
 		.pipe(concat('script.js'))
 		.pipe(browserify())
-		.pipe(gulp.dest('builds/development/js'))	
+		.pipe(gulp.dest('builds/development/js'))
+		.pipe(connect.reload())	
 });
-
-
 
 gulp.task('compass', function() {
   gulp.src(sassSources)
@@ -40,7 +40,7 @@ gulp.task('compass', function() {
     })
     .on('error', gutil.log))
     .pipe(gulp.dest('builds/development/css'))
-    
+    .pipe(connect.reload())	
 });
 
 gulp.task('watch', function() {
@@ -50,6 +50,14 @@ gulp.task('watch', function() {
 
 });
 
-gulp.task('default', ['coffee', 'js', 'compass', 'watch']);
+gulp.task('connect', function() {
+  connect.server({
+    root: 'builds/development/',
+    port: 8888,
+    livereload: true
+  });
+});
+
+gulp.task('default', ['coffee', 'js', 'compass', 'connect', 'watch']);
 
 
